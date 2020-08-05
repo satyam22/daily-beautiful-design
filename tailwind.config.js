41bde5f2b2de579dbb2d428aacc9e975e8568c59
Satyam Bansal <satyam@pesto.tech>
2020-08-05 16:31:43 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        '2/3': '66%',
+        '3/4': '75%',
+        '3/10': '30%',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
